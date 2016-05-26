69d0fc0f68891ae7a596a7451a8438adc1baec2f
Saurabh Kukade <saurabhkukade@live.com>
2016-05-26 20:16:27 +0530

    storing old values and new values on editTab

--- front_end/js/editJs.js
+++ front_end/js/editJs.js
@@ -1,34 +1,58 @@
 $(document).ready(function (){
-    getEditTable(localStorage.table,localStorage.tupple,tupValLs);
-    var old_table = getOldTableValues();
+    var old_table = getEditTable(localStorage.table,localStorage.tupple,tupValLs);
     $('#editTable').find('tr').first().find('input').focus();
     addNewRowBelow($('#editTable').find('tr').first().find('input'));
     $("input").keydown(function(e){
 	handleKeyEvent(e,$(this));
     });
     $('.submitButton').click(function(e){
-	window.location="tableNames.html";
+	// window.location="tableNames.html";
+	checkChanges(old_table);
+	
     });
 });
 
-function getOldTableValues(){
-    tList = [];
-    console.log($('#editTable').find('tr').find('input'))
+function checkChanges(old_table){
+    old_vals= [];
+    for(var i = 0; i < old_table[0].length; i++) {
+	tList = [];
+	for(var j = 0; j < old_table.length; j++) {
+	    tList.push(old_table[j][i]);
+	}
+	old_vals.push(tList);
+    }
+    var new_val = getValuesFromTable();
+    // now check va
+}
+
+function getValuesFromTable(){
+    new_vals= [];
+    console.log($('#editTable tr').length);
+    // to get index name set i = 0;
+    for (i = 1; i < $('#editTable tr').length; i++){
+	tList = []
+	for(j = 0; j < $('#editTable tr').eq(i).find('td').length; j++){
+	    tList.push($('#editTable tr').eq(i).find('td').find('input').eq(j).val());
+	}
+	new_vals.push(tList);
+    }
+    return new_vals;
+	
 }
 
 function addNewRowBelow(ths){
-    flag = 1
-    ls = ths.parent().parent().parent().find('input')
+    flag = 1;
+    ls = ths.parent().parent().parent().find('input');
     for(i=0;i<ls.length;i++){
 	if($(ls[i]).val()=="")
 	    flag = 0;
     }
     if(flag){
-	column = ""
-	ls = ths.parent().parent().find('input')
+	column = "";
+	ls = ths.parent().parent().find('input');
 	for(i=0;i<ls.length;i++)
-	    column = column + '<td><input type="text" value=""></td>'
-	obj = $('<tr>'+column+'</tr>;')
+	    column = column + '<td><input type="text" value=""></td>';
+	obj = $('<tr>'+column+'</tr>;');
 	obj.find('input').keydown(function(e){
 	    handleKeyEvent(e,$(this));
 	});
@@ -43,11 +67,11 @@ function handleKeyEvent(e,ths)
 	addNewRowBelow(ths);
     }
     if(keycode == '37'){
-	next = ths.parent().prev()
+	next = ths.parent().prev();
 	next.find('input').focus();
     }
     if(keycode == '39'){
-	next = ths.parent().next()
+	next = ths.parent().next();
 	next.find('input').focus();
     }
     if(keycode == '38'){
@@ -62,32 +86,33 @@ function handleKeyEvent(e,ths)
 
 function getEditTable(tableName,tupLs,tupValLs){
     tupLs = tupLs.split(",");
-    column = ""
+    column = "";
     for(i=0;i<tupLs.length;i++){
-	column = column + '<td> <input type="button" value="'+tupLs[i]+'"></td>'
+	column =column + '<td> <input type="button" value="'+tupLs[i]+'"></td>';
     }
-    $('#editTable').append('<tr>'+column+'</tr>;')
-    rowAttr = []
+    $('#editTable').append('<tr>'+column+'</tr>;');
+    rowAttr = [];
     for(i=0;i<tupValLs.length;i++){
 	if(tuppleLs[i][0]==tableName){
 	    for(j=0;j<tupLs.length;j++){
 		for(k=0;k<tupValLs[i][1].length;k++){
 		    if(tupValLs[i][1][k][0]==tupLs[j])
 			rowAttr.push(tupValLs[i][1][k][1]);
+			// console.log(tupValLs[i][1][k][1]);
 		}
 	    }
 	}
     }
-    addRow(rowAttr,tupLs)
+    addRow(rowAttr,tupLs);
+    return rowAttr;
 }
 
 function addRow(valLs,tupLs){
-    console.log(valLs)
     for(j=0;j<valLs[0].length;j++){
-	row = ""
+	row = "";
 	for(i=0;i<tupLs.length;i++){
-	    row = row + '<td> <input type="text" value="'+valLs[i][j]+'"></td>'
+	    row = row + '<td> <input type="text" value="'+valLs[i][j]+'"></td>';
 	}
-	$('#editTable').append('<tr>'+row+'</tr>;')
+	$('#editTable').append('<tr>'+row+'</tr>;');
     }
 }
